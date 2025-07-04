08201f2208aaaf9be93b1482ceb5bb72c3d3a45d
toyosi-1 <ajibolaolatoyosi21@gmail.com>
2025-07-04 20:08:24 +0100

    fix: remove fixed positioning and overflow hidden from root html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -140,12 +140,6 @@ export default function RootLayout({
         width: '100%',
         margin: 0,
         padding: 0,
-        overflow: 'hidden',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
       } as React.CSSProperties}
       suppressHydrationWarning
     >
